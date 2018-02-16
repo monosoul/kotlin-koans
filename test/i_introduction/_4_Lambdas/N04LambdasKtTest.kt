32f2e98d69a6523ce1073aa26762ba7fd4087ff0
monosoul <kloz.klaud@gmail.com>
2018-02-16 23:04:38 +0500

    resolve some more tests

--- test/i_introduction/_4_Lambdas/N04LambdasKtTest.kt
+++ test/i_introduction/_4_Lambdas/N04LambdasKtTest.kt
@@ -1,15 +1,16 @@
 package i_introduction._4_Lambdas
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class N04LambdasKtTest {
     @Test fun contains() {
-        assertTrue(task4(listOf(1, 2, 3)))
+        assertThat(task4(listOf(1, 2, 3))).isTrue();
     }
 
     @Test fun notContains() {
-        assertFalse(task4(listOf(1, 3, 5)))
+        assertThat(task4(listOf(1, 3, 5))).isFalse();
     }
 }
--- test/i_introduction/_5_String_Templates/N05StringTemplatesKtTest.kt
+++ test/i_introduction/_5_String_Templates/N05StringTemplatesKtTest.kt
@@ -1,20 +1,18 @@
 package i_introduction._5_String_Templates
 
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import java.util.regex.Pattern
 
 class N05StringTemplatesKtTest {
     @Test fun match() {
-        assertTrue("11 MAR 1952".matches(task5().toRegex()))
+        assertThat("11 MAR 1952").matches(task5());
     }
 
     @Test fun match1() {
-        assertTrue("24 AUG 1957".matches(task5().toRegex()))
+        assertThat("24 AUG 1957").matches(task5())
     }
 
     @Test fun doNotMatch() {
-        assertFalse("24 RRR 1957".matches(task5().toRegex()))
+        assertThat("24 RRR 1957").doesNotMatch(task5())
     }
 }
